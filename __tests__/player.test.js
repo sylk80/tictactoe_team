fc8b3a287f9b479a2f803055b9856415e4b67351
Julian Ostmo <julian.ostmo@ibm.com>
2022-03-15 10:49:20 +0000

    test: player x message - refactor - red

--- __tests__/player.test.js
+++ __tests__/player.test.js
@@ -2,9 +2,20 @@ const Player = require("../src/player")
 
 describe("Given a TicTacToe player", () => {
     describe("When the player is initialised", () => {
+        let player
+        const mark = "X"
+
+        beforeEach(() => {
+            player = new Player(mark)
+        })
+
         test("Then a new player object is created", () => {
             const player = new Player()
             expect(player).toBeInstanceOf(Player)
+        })
+
+        test("Then the player's mark is set", () => {
+            expect(player.mark).toEqual(mark)
         })
     })
 })
